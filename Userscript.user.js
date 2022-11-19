802f25eae3206121eca5e906eafad13400ba72ee
Pei Liu <79866816+peiliou@users.noreply.github.com>
2022-11-19 18:50:01 -0500

    Update Userscript.user.js

--- Userscript.user.js
+++ Userscript.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Wikipedia NLP
+// @name         Wikipedia Talk Parser
 // @namespace    http://tampermonkey.net/
 // @version      1.0.0
 // @description  try to take over the world!
